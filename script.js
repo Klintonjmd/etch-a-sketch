84519f3e7f0bbf67545f2f62cf8d1724833c4374
Klinton-VM <100642525+Klintonjmd@users.noreply.github.com>
2023-08-12 21:38:12 -0400

    Outline desired functionality in scipt.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// BASE FUNCTIONALITY
+// We need a host div
+// create a 256 boxes using JS as children of that div
+// move those boxes into a 16x16 grid via flexbox
+// create a "hover" event listener for the boxes which updates the color to black via JS
+
+// GRID CUSTOMIZATION
+// add an input where the user can enter a number for the grid size
+// check the min between 100 and userinput
